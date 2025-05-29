0f80cad9e4faa3876f5e5cef5bffd0d059db5fe4
CodeCaesar <adrian@czerwinski.info.pl>
2025-05-29 16:36:06 +0100

    Created private Node class in SinglyLinkedList.java

--- javaDS/SinglyLinkedList.java
+++ javaDS/SinglyLinkedList.java
@@ -1,4 +1,25 @@
 package javaDS;
 
 public class SinglyLinkedList {
+
+    private class Node {
+        private int data;
+        private Node nextNode = null;
+
+        private Node(int data) {
+            this.data = data;
+        }
+
+        @Override
+        public String toString() {
+            return "[" + this.data + "]-> " + this.nextNode.toString();
+        }
+    }
+
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public SinglyLinkedList() {
+    }
 }
