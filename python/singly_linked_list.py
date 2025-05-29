3809bf909f9782871fcdf8cb9cbd1c7e9f6e6b9a
CodeCaesar <adrian@czerwinski.info.pl>
2025-05-29 14:32:28 +0100

    Made some useful methods for SinglyLinkedList

--- python/singly_linked_list.py
+++ python/singly_linked_list.py
@@ -19,3 +19,15 @@ class SinglyLinkedList:
         self.head = None
         self.tail = None
         self.size = 0
+    
+    def is_empty(self):
+        return self.size == 0
+    
+    def get_size(self):
+        return self.size
+    
+    def get_head(self):
+        return self.head
+    
+    def get_tail(self):
+        return self.tail
