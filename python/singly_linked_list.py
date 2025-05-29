2a2690f34179c7d0eec3f1c0e467056885e1eb72
CodeCaesar <adrian@czerwinski.info.pl>
2025-05-29 15:02:18 +0100

    Made repr for SInglyLinkedList and changed string representation of _Node

--- python/singly_linked_list.py
+++ python/singly_linked_list.py
@@ -7,7 +7,7 @@ class _None:
         self.next_node = None
     
     def __repr__(self):
-        return f"(Data: {self.data}) -> {self.next_node}"
+        return f"[Data: {self.data}]-> {self.next_node}"
 
 class SinglyLinkedList:
 
@@ -19,6 +19,28 @@ class SinglyLinkedList:
         self.head = None
         self.tail = None
         self.size = 0
+    
+    def __repr__(self):
+        """
+        Representation of SinglyLinkedList returns string of all nodes, but if the node is head or tail,
+        it will make it have Head or Tail at the end respectively.
+
+        Running Time: O(1) + O(1) + O(n) + O(n) + O(n) + O(n) + O(n) + O(n) + O(n) + O(1) = O(7n + 3) = O(n)
+        But every linked list has only one head and one tail, thus it appends head and tail only once
+        Running Time: O(1) + O(1) + O(n) + O(n) + O(1) + O(n) + O(1) + O(n) + O(n) + O(1) = O(5n + 5) = O(n)
+        """
+        nodes = []
+        current = self.head
+
+        while current:
+            if current is self.head:
+                nodes.append(f"Head[Data: {current.data}]")
+            elif current is self.tail:
+                nodes.append(f"Tail[Data: {current.data}]")
+            else:
+                nodes.append(f"[Data: {current.data}]")
+        
+        return f"Linked List: ({'-> '.join(nodes)})"
     
     def is_empty(self):
         return self.size == 0
@@ -48,5 +70,5 @@ class SinglyLinkedList:
         new_node = _None(value)
         new_node.next_node = self.head
         self.head = new_node
-        
+
         self.size += 1
