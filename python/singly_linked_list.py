b81e89502a31e7b91c56aef0ecf96657d28593c1
CodeCaesar <adrian@czerwinski.info.pl>
2025-05-29 15:59:21 +0100

    Fixed typos where _Node was _None, __init__ was __self__, and forgot to write 'current = current.next_node', also made remove function

--- python/singly_linked_list.py
+++ python/singly_linked_list.py
@@ -1,8 +1,7 @@
-class _None:
+class _Node:
     data: int
-    next_node: _None
 
-    def __self__(self, data):
+    def __init__(self, data):
         self.data = data
         self.next_node = None
     
@@ -11,11 +10,11 @@ class _None:
 
 class SinglyLinkedList:
 
-    head: _None
-    tail: _None
+    head: _Node
+    tail: _Node
     size: int
 
-    def __self__(self):
+    def __init__(self):
         self.head = None
         self.tail = None
         self.size = 0
@@ -25,9 +24,9 @@ class SinglyLinkedList:
         Representation of SinglyLinkedList returns string of all nodes, but if the node is head or tail,
         it will make it have Head or Tail at the end respectively.
 
-        Running Time: O(1) + O(1) + O(n) + O(n) + O(n) + O(n) + O(n) + O(n) + O(n) + O(1) = O(7n + 3) = O(n)
-        But every linked list has only one head and one tail, thus it appends head and tail only once
-        Running Time: O(1) + O(1) + O(n) + O(n) + O(1) + O(n) + O(1) + O(n) + O(n) + O(1) = O(5n + 5) = O(n)
+        Running Time: O(1) + O(1) + O(n) + O(n) + O(n) + O(n) + O(n) + O(n) + O(n) + O(n) + O(1) = O(8n + 3) = O(n)
+        But every linked list has only one head and one tail, thus it appends head and tail only once.
+        Running Time: O(1) + O(1) + O(n) + O(n) + O(1) + O(n) + O(1) + O(n) + O(n) + O(n) + O(1) = O(6n + 5) = O(n)
         """
         nodes = []
         current = self.head
@@ -39,6 +38,8 @@ class SinglyLinkedList:
                 nodes.append(f"Tail[Data: {current.data}]")
             else:
                 nodes.append(f"[Data: {current.data}]")
+            
+            current = current.next_node
         
         return f"Linked List: ({'-> '.join(nodes)})"
     
@@ -55,7 +56,7 @@ class SinglyLinkedList:
         return self.tail
     
     def append(self, value):
-        new_node = _None(value)
+        new_node = _Node(value)
 
         if self.head == None:
             self.head = new_node
@@ -67,8 +68,49 @@ class SinglyLinkedList:
         self.size += 1
     
     def prepend(self, value):
-        new_node = _None(value)
+        new_node = _Node(value)
         new_node.next_node = self.head
         self.head = new_node
 
         self.size += 1
+    
+    def remove(self, value):
+        if self.size == 0:
+            return "Linked List is empty"
+
+        current = self.head
+
+        if self.size == 1:
+            if current.data == value:
+                self.head = None
+                self.tail = None
+                self.size -= 1
+                return
+            else:
+                return f'Node with value: "{value}" not found'
+
+        if current.data == value:
+            self.head = self.head.next_node
+            self.size -= 1
+
+        while current.next_node:
+            if current.next_node.data == value:
+                current.next_node = current.next_node.next_node
+                self.size -= 1
+
+            current = current.next_node
+        
+        return f'Node with value: "{value}" not found'
+
+
+list = SinglyLinkedList()
+list.append(5)
+list.append(6)
+list.append(2)
+list.append(9)
+list.append(4)
+list.append(3)
+list.remove(5)
+list.remove(4)
+
+print(list)
