9f061b15a1cc83e0aba92b384ea183a0a787b370
CodeCaesar <adrian@czerwinski.info.pl>
2025-06-03 00:08:00 +0100

    Made has method

--- javaDS/Trees/BinarySearchTree.java
+++ javaDS/Trees/BinarySearchTree.java
@@ -3,6 +3,22 @@ package javaDS.Trees;
 public class BinarySearchTree extends BinaryTree {
     public BinarySearchTree() {}
 
+    private boolean has(int key, Node current) {
+        if(current == null) {
+            return false;
+        } else if(current.data == key) {
+            return true;
+        } else if(key < current.data) {
+            return has(key, current.left);
+        } else {
+            return has(key, current.right);
+        }
+    }
+
+    public boolean has(int key) {
+        return has(key, this.root);
+    }
+
     public int min() {
         Node current = this.root;
 
