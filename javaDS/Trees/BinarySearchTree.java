cff6fb1f3cbd77ae15761262eaea8c97d3d97c4c
CodeCaesar <adrian@czerwinski.info.pl>
2025-06-02 23:54:44 +0100

    Made min method

--- javaDS/Trees/BinarySearchTree.java
+++ javaDS/Trees/BinarySearchTree.java
@@ -2,4 +2,14 @@ package javaDS.Trees;
 
 public class BinarySearchTree extends BinaryTree {
     public BinarySearchTree() {}
+
+    public int min() {
+        Node current = this.root;
+
+        while(current != null) {
+            current = current.left;
+        }
+
+        return current.data;
+    }
 }
