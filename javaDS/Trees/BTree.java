a17c186e7581e298b74b855ff824a6095ff0b0f6
CodeCaesar <adrian@czerwinski.info.pl>
2025-06-05 00:17:33 +0100

    Made has method that returns true if BTree has Node with key tragetKey

--- javaDS/Trees/BTree.java
+++ javaDS/Trees/BTree.java
@@ -1,17 +1,19 @@
 package javaDS.Trees;
 
-public abstract class BTree extends BinarySearchTree {
+public class BTree extends BinaryTree {
+    protected Node root;
+    protected int size;
     protected int minDegree;
     protected int maxDegree;
 
     protected class Node {
         protected int keysStored;
-        protected int[] key = new int[maxDegree - 1];
+        protected int[] keys = new int[maxDegree - 1];
         protected Node[] children = new Node[maxDegree];
         protected boolean leaf;
 
-        protected Node(int[] key) {
-            this.key = key;
+        protected Node(int[] keys) {
+            this.keys = keys;
         }
 
         @Override
@@ -23,4 +25,24 @@ public abstract class BTree extends BinarySearchTree {
     public BTree() {}
 
     public BTree(int minDegree) {this.minDegree = minDegree; this.maxDegree = minDegree * 2 - 1;}
+
+    private boolean has(Node current, int targetKey) {
+        int index = 0;
+
+        while(index < current.keysStored && targetKey > current.keys[index]) {
+            index += 1;
+        }
+
+        if(index < current.keysStored && targetKey == current.keys[index]) {
+            return true;
+        } else if(current.leaf) {
+            return false;
+        } else {
+            return has(current.children[index], targetKey);
+        }
+    }
+
+    public boolean has(int targetKey) {
+        return has(this.root, targetKey);
+    }
 }
