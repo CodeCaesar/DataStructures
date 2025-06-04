6ff15559897fb7a28d67fc92f108727627908cdb
CodeCaesar <adrian@czerwinski.info.pl>
2025-06-04 15:21:31 +0100

    Created RedBlackTree.java class and made initialisation for left and right rotate methods

---
+++ javaDS/Trees/RedBlackTree.java
@@ -0,0 +1,9 @@
+package javaDS.Trees;
+
+public class RedBlackTree extends BinarySearchTree {
+    public RedBlackTree() {}
+
+    private void leftRotate() {}
+
+    private void rightRotate() {}
+}
