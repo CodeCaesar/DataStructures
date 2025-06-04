50c7b0b31bc4c438adc46d08bbc74256359610ba
CodeCaesar <adrian@czerwinski.info.pl>
2025-06-04 23:54:32 +0100

    Made Node class for BTree.java

--- javaDS/Trees/BTree.java
+++ javaDS/Trees/BTree.java
@@ -1,5 +1,26 @@
 package javaDS.Trees;
 
 public abstract class BTree extends BinarySearchTree {
-    
+    protected int minDegree;
+
+    protected class Node<Data> {
+        protected int keysStored;
+        protected int[] key = new int[minDegree * 2 - 1];
+        protected Data data;
+        protected Node[] children;
+
+        protected Node(int[] key, Data data) {
+            this.key = key;
+            this.data = data;
+        }
+
+        @Override
+        public String toString() {
+            return "";
+        }
+    }
+
+    public BTree() {}
+
+    public BTree(int minDegree) {this.minDegree = minDegree;}
 }
