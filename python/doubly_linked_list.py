039edcc95714223159cc233ceaaa18639d8c3225
CodeCaesar <adrian@czerwinski.info.pl>
2025-06-05 23:18:12 +0100

    Made basic functions for DLL

--- python/doubly_linked_list.py
+++ python/doubly_linked_list.py
@@ -18,3 +18,35 @@ class DoublyLinkedList:
         self.head = None
         self.tail = None
         self.size = 0
+    
+    def is_empty(self):
+        """
+        Checks if Linked List is empty.
+
+        Running Time: <b>O(1)</b>
+        """
+        return self.size == 0
+    
+    def get_size(self):
+        """
+        Returns current size of Linked List.
+        
+        Running Time: <b>O(1)</b>
+        """
+        return self.size
+    
+    def get_head(self):
+        """
+        Returns head Node's value of Linked List.
+        
+        Running Time: <b>O(1)</b>
+        """
+        return self.head.data
+    
+    def get_tail(self):
+        """
+        Returns tail Node's value of Linked List.
+
+        Running Time: <b>O(1)</b>
+        """
+        return self.tail.data
