0e9b59f9058b8ecfc56523a1e38ce52fdc43ee19
CodeCaesar <adrian@czerwinski.info.pl>
2025-06-06 01:44:54 +0100

    Made height function for BT

--- python/trees/binary_tree.py
+++ python/trees/binary_tree.py
@@ -29,3 +29,20 @@ class BinaryTree:
 
     def get_root(self):
         return self.root.key
+    
+    def max_height(self, height1, height2):
+        if height1 >= height2:
+            return height1
+        else:
+            return height2    
+
+    def height(self, current):
+        if not current:
+            return 0
+        elif not current.left and not current.right:
+            return 0
+
+        return self.max_height(self.height(current.left), self.height(current.right)) + 1
+
+    def height(self):
+        return self.height(self.root)
