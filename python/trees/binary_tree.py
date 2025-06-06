0f7578c5908721887bbbd41a1d35dbd1175fae7c
CodeCaesar <adrian@czerwinski.info.pl>
2025-06-06 01:45:35 +0100

    Made inorder, preorder, postorder functions for BT

--- python/trees/binary_tree.py
+++ python/trees/binary_tree.py
@@ -46,3 +46,47 @@ class BinaryTree:
 
     def height(self):
         return self.height(self.root)
+
+    def __inorder(self, current):
+        if current:
+            self.__inorder(current.left)
+            print(current.key)
+            self.__inorder(current.right)
+    
+    def inorder(self):
+        self.__inorder(self.root)
+
+    def __preorder(self, current):
+        if current:
+            print(current.key)
+            self.__preorder(current.left)
+            self.__preorder(current.right)
+    
+    def preorder(self):
+        self.__preorder(self.root)
+
+    def __postorder(self, current):
+        if current:
+            self.__postorder(current.left)
+            self.__postorder(current.right)
+            print(current.key)
+    
+    def postorder(self):
+        self.__postorder(self.root)
+
+
+BT = BinaryTree()
+BT.root = _Node(2, "")
+BT.root.left = _Node(5, "")
+BT.root.right = _Node(9, "")
+BT.root.left.left = _Node(142, "")
+BT.root.right.right = _Node(3, "")
+
+print(BT)
+
+print("INORDER")
+BT.inorder()
+print("PREORDER")
+BT.preorder()
+print("POSTORDER")
+BT.postorder()
