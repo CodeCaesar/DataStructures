d42114aa49943dc161fb9b8f47010390c5b5b949
CodeCaesar <adrian@czerwinski.info.pl>
2025-06-06 01:42:17 +0100

    Created BinaryTree class for binary_tree.py with basic functions

--- python/trees/binary_tree.py
+++ python/trees/binary_tree.py
@@ -11,3 +11,21 @@ class _Node:
 
     def __repr__(self):
         return f"({self.left}) <-[{self.key}]-> ({self.right})"
+
+class BinaryTree:
+    
+    root : _Node
+    size : int
+
+    def __init__(self):
+        self.root = None
+        self.size = 0
+    
+    def __repr__(self):
+        return f"{self.root}"
+
+    def get_size(self):
+        return self.size
+
+    def get_root(self):
+        return self.root.key
