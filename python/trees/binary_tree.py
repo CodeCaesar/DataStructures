d5ea513a313037aaf706032fe3508eb497143b1b
CodeCaesar <adrian@czerwinski.info.pl>
2025-06-06 01:39:12 +0100

    Created private _Node class for binary_tree.py

---
+++ python/trees/binary_tree.py
@@ -0,0 +1,13 @@
+class _Node:
+    
+    key :int
+
+    def __init__(self, key, data):
+        self.key = key
+        self.data = data
+        self.parent = None
+        self.left = None
+        self.right = None
+
+    def __repr__(self):
+        return f"({self.left}) <-[{self.key}]-> ({self.right})"
