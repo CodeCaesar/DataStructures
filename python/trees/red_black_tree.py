45dd89a247115521189f435781a971a9d7ded479
CodeCaesar <adrian@czerwinski.info.pl>
2025-06-08 22:26:26 +0100

    Created _Node class for RBT

--- python/trees/red_black_tree.py
+++ python/trees/red_black_tree.py
@@ -1,5 +1,20 @@
-class RedBlackTree(BinaryTree):
+class _Node:
     
+    key : int
+
+    def __init__(self, key, data, colour="Red"):
+        self.key = key
+        self.data = data
+        self.parent = None
+        self.left = None
+        self.right = None
+        self.colour = colour
+
+    def __repr__(self):
+        return f"({self.left}) <-[{self.key}:{self.colour}]-> ({self.right})"
+
+class RedBlackTree(BinaryTree):
+
     size : int
     root : _Node
 
