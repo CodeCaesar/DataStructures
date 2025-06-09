e1e0ff22e016bd9a177625c105514f9ec20a9266
CodeCaesar <adrian@czerwinski.info.pl>
2025-06-09 23:42:04 +0100

    Made basic methods for DLL

--- javaDS/DoublyLinkedList.java
+++ javaDS/DoublyLinkedList.java
@@ -5,6 +5,7 @@ public class DoublyLinkedList {
     private class Node {
         private int data;
         private Node nextNode = null;
+        private Node prevNode = null;
 
         private Node(int data) {
             this.data = data;
@@ -21,5 +22,41 @@ public class DoublyLinkedList {
     private int size;
 
     public DoublyLinkedList() {
+    }
+
+    /**
+     * Checks if Linked List is empty.
+     * <p>
+     * Running Time: <b>O(1)</b>
+     */
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+
+    /**
+     * Returns current size of Linked List.
+     * <p>
+     * Running Time: <b>O(1)</b>
+     */
+    public int getSize() {
+        return this.size;
+    }
+
+    /**
+     * Returns head Node's value of Linked List.
+     * <p>
+     * Running Time: <b>O(1)</b>
+     */
+    public int getHead() {
+        return this.head.data;
+    }
+
+    /**
+     * Returns tail Node's value of Linked List.
+     * <p>
+     * Running Time: <b>O(1)</b>
+     */
+    public int getTail() {
+        return this.tail.data;
     }
 }
